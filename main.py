74cd7b07c4844b6cdc3e820666eb9476b10f270f
Dylan Penney <dylan@eduroam-int-dhcp-98-159-124.ucl.ac.uk>
2022-03-13 14:20:16 +0000

    general improvement

--- main.py
+++ main.py
@@ -2,31 +2,21 @@ import random
 
 class Main:
 	def __init__(self):
-		self.get_word_question()
-		self.get_warm_up_question()
+		pass
 
+	# Provides interface with a random question from the selection of warm up questions
 	def get_warm_up_question(self):
 		with open('warm-up-questions.txt') as f:
 			lines = f.readlines()
 			return self.format_raw_question(random.choice(lines))
-			"""
-			for i in range(0, len(lines)): # doesn't work when using enhanced for??
-				lines[i] = self.format_raw_question(lines[i])
-				#print(lines[i])
-			"""
 
 	def get_word_question(self):
 		with open('questions.txt') as f:
 			lines = f.readlines()		
 			return self.format_raw_question(random.choice(lines))
-			"""
-			for i in range(0, len(lines)):
-				lines[i] = self.format_raw_question(lines[i])
-				#print(lines[i])
-			"""
 
 	def format_raw_question(self, line):
 		line = str(line).encode().decode('unicode-escape') #.replace("|", "\n")
-		return line.format(0.86, 0.54, "AUB", 0.62, "A")
+		return line
 
 Main()
