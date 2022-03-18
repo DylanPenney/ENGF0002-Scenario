6dcd8135cca2445c90ea2b41bcd31556142a6b90
Dylan Penney <dylan@eduroam-int-dhcp-97-121-164.ucl.ac.uk>
2022-03-18 12:07:43 +0000

    added solution reading

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import random
 
 class Main:
 	def __init__(self):
-		pass
+		print(self.get_warm_up_question())
 
 	# Provides interface with a random question from the selection of warm up questions
 	def get_warm_up_question(self):
-		with open('warm-up-questions.txt') as f:
-			lines = f.readlines()
-			return self.format_raw_question(random.choice(lines))
+		with open('warm-up-questions.txt') as q:
+			questions = q.readlines()
+		with open('warm-up-solutions.txt') as s:
+			solutions = s.readlines()
+		question = self.format_raw_question(random.choice(questions))
+		solution = self.format_raw_question(random.choice(solutions))
+		return question, solution
 
 	def get_word_question(self):
 		with open('questions.txt') as f:
