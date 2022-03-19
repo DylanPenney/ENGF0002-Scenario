05e2aa46d367b7de8977a685a8ef9a430163cf23
Dylan Penney <dylan@eduroam-int-dhcp-97-121-164.ucl.ac.uk>
2022-03-19 18:31:44 +0000

    really janky v1

--- main.py
+++ main.py
@@ -2,13 +2,42 @@ import random
 
 class Main:
 	def __init__(self):
-		print(self.get_word_question())
+		self.warm_up_question()
+		self.word_question()
+
+	def warm_up_question(self):
+		A = round(random.uniform(0.1, 1.0), 2)
+		B = round(random.uniform(0.1, 1.0), 2)
+		AorB = round(random.uniform(0.1, 1.0), 2)
+		AandB = round(A + B - AorB, 2)
+		AgivenB = round(AandB / B, 2)
+		pair = self.get_warm_up_question()
+
+		question = pair[0].replace("PA", str(A)).replace("PB", str(B)).replace("AorB", str(AorB)).replace("AandB", str(AandB)).replace("AgivenB", str(AgivenB))
+		solution = round(eval(pair[1]), 2)
+		user_answer = -1 # Turn while loop into a 'do-while' loop
+		print(question)
+		while not self.check_solution(user_answer, solution):
+			user_answer = float(input("Answer to two decimal places (where applicable).\n>>>"))
+		print("Correct")
+
+	def word_question(self):
+		random_numbers = [random.randint(1, 10) for i in range(5)]
+		pair = self.get_word_question()
+
+		question = pair[0].replace("R1", str(random_numbers[0])).replace("R2", str(random_numbers[1]))
+		solution = round(eval(pair[1]), 2)
+		user_answer = -1 # Turn while loop into a 'do-while' loop
+		print(question)
+		while not self.check_solution(user_answer, solution):
+			user_answer = float(input("Answer to two decimal places (where applicable).\n>>>"))
+		print("Correct")
 
 	# Provides interface with a random question and answer from the selection of warm up questions
 	def get_warm_up_question(self):
-		with open('warm-up-questions/warm-up-questions.txt') as q:
+		with open('warm-up-questions/warm-up-questions.txt', 'r') as q:
 			questions = q.readlines()
-		with open('warm-up-questions/warm-up-solutions.txt') as s:
+		with open('warm-up-questions/warm-up-solutions.txt', 'r') as s:
 			solutions = s.readlines()
 		index = random.randint(0, min(len(questions), len(solutions) - 1))
 		question = self.format_raw_question(questions[index])
@@ -26,17 +55,13 @@ class Main:
 		return question, solution
 
 	def format_raw_question(self, line):
-		line = str(line)[:-1].encode().decode('utf-8')[:-1]
+		line = str(line).encode().decode('utf-8').replace('\n', '')
 		return line
 	def format_raw_solution(self, line):
-		line = str(line).encode().decode('utf-8')[:-1]
+		line = str(line).encode().decode('utf-8').replace('\n', '')
 		return line
 
 	def check_solution(self, user_answer, solution):
-		if user_answer == solution:
-			print("You got it right!")
-			return True
-		print("Incorrect!")
-		return False
+		return user_answer == solution
 
 Main()
