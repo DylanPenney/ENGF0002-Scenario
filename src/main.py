8d32b42222e2a8a586f4999e26a3e362937fe426
Dylan Penney <penneydylan@icloud.com>
2022-03-20 14:57:46 +0000

    follow rules of proportionality

--- src/main.py
+++ src/main.py
@@ -7,15 +7,17 @@ class Main:
 
 	# Acts as a menu for questions (warm-up)
 	def warm_up_question(self):
-		A = round(random.uniform(0.1, 1.0), 2)
-		B = round(random.uniform(0.1, 1.0), 2)
-		AorB = round(A + B, 2)
-		AandB = round(A * B, 2)
-		AgivenB = round(AandB / B, 2)
+		A = self.round_to_dp(random.uniform(0.1, 1.0), 2)
+		B = self.round_to_dp(random.uniform(0.1, 1.0), 2)
+		print(self.round_to_dp(A, 2), self.round_to_dp(B, 2))
+		AandB = self.round_to_dp(A * B, 2)
+		AorB = self.round_to_dp(A + B - AandB, 2)
+		AgivenB = A
+		BgivenA = self.round_to_dp(B, 2)
 		pair = self.get_warm_up_question()
 
 		question = pair[0].replace("PA", str(A)).replace("PB", str(B)).replace("AorB", str(AorB)).replace("AandB", str(AandB)).replace("AgivenB", str(AgivenB))
-		solution = round(eval(pair[1]), 2)
+		solution = self.round_to_dp(eval(pair[1]), 2)
 		self.check_solution(question, solution)
 
 	# Acts as a menu for questions (word)
@@ -24,7 +26,7 @@ class Main:
 		pair = self.get_word_question()
 
 		question = pair[0].replace("R1", str(random_numbers[0])).replace("R2", str(random_numbers[1]))
-		solution = round(eval(pair[1]), 2)
+		solution = self.round_to_dp(eval(pair[1]), 2)
 		self.check_solution(question, solution)
 
 	# Provides interface with a random question and answer from the selection of warm up questions
@@ -58,10 +60,15 @@ class Main:
 	def check_solution(self, question, solution):
 		user_answer = -1 # Turn while loop into a 'do-while' loop
 		print(question)
+		print(solution)
 		print("Answer to two decimal places (where applicable).")
-		user_answer = round(float(input(">>>")), 2)
+		user_answer = self.round_to_dp(float(input(">>>")), 2)
 		while user_answer != solution:
 			user_answer = float(input("Incorrect!\n>>>"))
 		print("Correct!")
+
+	def round_to_dp(self, val, digits=2):
+		precision = 10 ** digits
+		return (round(precision * val) / precision)
 
 Main()
